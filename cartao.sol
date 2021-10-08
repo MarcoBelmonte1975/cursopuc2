ca10d6ca68b250f3b05695cdfe5098346f92d154
MarcoBelmonte1975 <92129843+MarcoBelmonte1975@users.noreply.github.com>
2021-10-07 22:23:06 -0300

    Create cartao.sol

---
+++ cartao.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.8.9;
+
+contract cartaodevacinacao { 
+ // autor: marco 
+ string constant public nome = "Marco Belmonte";
+ string public dose = "primeira";
+ string public unidade = "xxx";
+ uint public numeroCNES = 1000;
+ uint public data = 71002021; 
+ uint constant private lote = 12345; 
+ string constant private fabricante = "astra";
+ string public vacinador = "fulano"; 
+ bool public imunizado = false; 
+ address public wallet = 0xca1257E7eCDd753032721f4c7525B7C2497fbAD9;
+ 
+}
